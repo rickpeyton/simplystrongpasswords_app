f0d4378d3d66938e41cb9e71014c4651d46f6d76
Rick Peyton <peytorb@gmail.com>
2014-12-06 07:00:15 -0600

    Seeded 5000 words and got basic output working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "hello world!"
+    password = Word.all.shuffle.take(3).collect{ |w| w.word }.join(' ')
+    render text: "#{password}"
   end
 end
