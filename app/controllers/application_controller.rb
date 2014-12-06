bd0e25dc10e9f97e70dbcde69a9950922f36f42a
Rick Peyton <peytorb@gmail.com>
2014-12-06 16:29:09 -0600

    Added static pages and a little bit of styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def hello
-    password = Word.all.shuffle.take(3).collect{ |w| w.word }.join(' ')
-    render text: "#{password}"
-  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
+    @password = Word.all.shuffle.take(3).collect{ |w| w.word }.join(' ')
   end
 end
